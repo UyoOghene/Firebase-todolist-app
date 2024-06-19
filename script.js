2eb22b9c8a43937abdcfbe803196d51b5fb9c90b
Uyooghene <uyooghene01@gmail.com>
2024-06-19 22:03:48 +0100

    connected to database

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref , push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://playground-f6f18-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(appSettings);
+const dataBase = getDatabase(app);
+const shoppingListInDB = ref(dataBase, "shoppingList")
+
+
 const inputField = document.querySelector('#input-field');
 const addButton = document.querySelector('#add-button');
 const inputForm = document.querySelector('#add-btn-field');
@@ -10,7 +22,8 @@ const addToCart = (e) => {
     const shoppingItem = document.createElement('p');
     shoppingItem.setAttribute('id','shoppingItemP');
     shoppingItemList.appendChild(shoppingItem);
-    shoppingItem.innerText = inputField.value;   
+    shoppingItem.innerText = inputField.value;  
+    push(shoppingListInDB , inputField.value)
     inputField.value = ''; 
 
 }
