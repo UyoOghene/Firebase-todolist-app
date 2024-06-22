503648bce51d0c16dbec21f367e4b4c03487922e
Uyooghene <uyooghene01@gmail.com>
2024-06-23 00:37:28 +0100

    ok

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref , push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref , onValue , push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://playground-f6f18-default-rtdb.firebaseio.com/"
@@ -29,3 +29,37 @@ const addToCart = (e) => {
 }
 
 inputForm.addEventListener('submit', addToCart);
+
+onValue(shoppingListInDB, function(snapshot){
+    let ArrayList = Object.values(snapshot.val());
+    console.log(ArrayList);
+    for(let i=0; i< ArrayList.length; i++){
+        console.log(ArrayList[i]);
+        
+    }
+})
+
+// challenge (object to array)
+// 1:Create a variable called scrimbausersemail and use one of the object methods to set it to an array with the values
+// 2: Create a variable called scrimba userids ans use one of the Object methods to set it equal to an array with the keys
+// 3: create a let variable called scrimbauserEntries and use one of the object methods to set it equal to an arrahy with both the keys and values
+
+// 1
+// let scrimbaUsers = {
+//     'uyo' : "uyo@gmail.com",
+//     'james' : "james@yahoo.com",
+//     'kaka' : "kaka@outlook.com"
+// };
+
+// let scrimbausersemail = Object.values(scrimbaUsers);
+// console.log(scrimbausersemail);
+
+// // 2
+// let scrimbaUsersIds = Object.keys(scrimbaUsers);
+// console.log(scrimbaUsersIds);
+
+// // 3
+// let scrimbauserEntries = Object.entries(scrimbaUsers);
+// console.log(scrimbauserEntries);
+
+ 
